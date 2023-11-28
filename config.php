8d6d3f15552351ee26c11b0beede8d53eede22cc
drianns <145427873+drianns@users.noreply.github.com>
2023-11-28 13:49:48 +0700

    Update config.php

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ $dbhost = 'localhost';
 $dbuser = 'root';
 $dbpass = '';
 $dbname = 'bukutamu';
-$conn = mysqli_connect($dbhost,$dbuser,$dbpass) or die('Error connecting to mysql');
+$conn = mysqli_connect($dbhost ,$dbuser ,$dbpass ) or die('Error connecting to mysql');
 mysqli_select_db($conn, $dbname);
 
 ?>
